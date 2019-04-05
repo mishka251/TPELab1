541fb24646ce7657ae869647da3971d2b7cf5dab
Mikhail Belov <mbelov@os-company.ru>
2019-04-05 14:31:46 +0500

    исправления для сдачи

--- TPELab1/Form1.Designer.cs
+++ TPELab1/Form1.Designer.cs
@@ -31,31 +31,31 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nuP = new System.Windows.Forms.NumericUpDown();
             this.label2 = new System.Windows.Forms.Label();
             this.Расчёт = new System.Windows.Forms.Button();
             this.nuN = new System.Windows.Forms.NumericUpDown();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.nuP = new System.Windows.Forms.NumericUpDown();
-            this.label3 = new System.Windows.Forms.Label();
-            this.nuX1 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.nuX2 = new System.Windows.Forms.NumericUpDown();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.nuX1 = new System.Windows.Forms.NumericUpDown();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nuP)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nuN)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nuP)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nuX1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nuX2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuX1)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -65,7 +65,7 @@
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(776, 357);
+            this.tabControl1.Size = new System.Drawing.Size(776, 420);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -77,38 +77,29 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(768, 331);
+            this.tabPage1.Size = new System.Drawing.Size(768, 394);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Полнофакторный";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // label1
             // 
-            this.tabPage2.Controls.Add(this.label5);
-            this.tabPage2.Controls.Add(this.label4);
-            this.tabPage2.Controls.Add(this.nuX2);
-            this.tabPage2.Controls.Add(this.nuX1);
-            this.tabPage2.Controls.Add(this.label3);
-            this.tabPage2.Controls.Add(this.nuP);
-            this.tabPage2.Controls.Add(this.label2);
-            this.tabPage2.Controls.Add(this.Расчёт);
-            this.tabPage2.Controls.Add(this.nuN);
-            this.tabPage2.Controls.Add(this.dataGridView2);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(768, 331);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Дробнофакторный";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "n";
             // 
-            // dataGridView1
+            // button1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(43, 135);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(667, 186);
-            this.dataGridView1.TabIndex = 0;
+            this.button1.Location = new System.Drawing.Point(369, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Расчёт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // numericUpDown1
             // 
@@ -133,25 +124,61 @@
             0});
             this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
-            // button1
+            // dataGridView1
             // 
-            this.button1.Location = new System.Drawing.Point(369, 50);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Расчёт";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Visible = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(43, 135);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(667, 186);
+            this.dataGridView1.TabIndex = 0;
             // 
-            // label1
+            // tabPage2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(94, 56);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(13, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "n";
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.nuX2);
+            this.tabPage2.Controls.Add(this.nuX1);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.nuP);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.Расчёт);
+            this.tabPage2.Controls.Add(this.nuN);
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 394);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Дробнофакторный";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(247, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "p=";
+            // 
+            // nuP
+            // 
+            this.nuP.Location = new System.Drawing.Point(291, 34);
+            this.nuP.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nuP.Name = "nuP";
+            this.nuP.Size = new System.Drawing.Size(120, 20);
+            this.nuP.TabIndex = 8;
+            this.nuP.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nuP.ValueChanged += new System.EventHandler(this.nuP_ValueChanged);
+            this.nuP.ChangeUICues += new System.Windows.Forms.UICuesEventHandler(this.numericUpDown1_ValueChanged);
             // 
             // label2
             // 
@@ -170,7 +197,6 @@
             this.Расчёт.TabIndex = 6;
             this.Расчёт.Text = "Расчёт";
             this.Расчёт.UseVisualStyleBackColor = true;
-            this.Расчёт.Visible = false;
             this.Расчёт.Click += new System.EventHandler(this.button2_Click);
             // 
             // nuN
@@ -190,7 +216,7 @@
             this.nuN.Size = new System.Drawing.Size(120, 20);
             this.nuN.TabIndex = 5;
             this.nuN.Value = new decimal(new int[] {
-            1,
+            2,
             0,
             0,
             0});
@@ -198,60 +224,35 @@
             // 
             // dataGridView2
             // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView2.Location = new System.Drawing.Point(51, 115);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.Size = new System.Drawing.Size(667, 186);
             this.dataGridView2.TabIndex = 4;
             // 
-            // nuP
+            // label6
             // 
-            this.nuP.Location = new System.Drawing.Point(291, 34);
-            this.nuP.Maximum = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.nuP.Name = "nuP";
-            this.nuP.Size = new System.Drawing.Size(120, 20);
-            this.nuP.TabIndex = 8;
-            this.nuP.ValueChanged += new System.EventHandler(this.nuP_ValueChanged);
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 358);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "X2";
             // 
-            // label3
+            // label7
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(247, 36);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(19, 13);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "p=";
-            // 
-            // nuX1
-            // 
-            this.nuX1.Location = new System.Drawing.Point(502, 36);
-            this.nuX1.Maximum = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.nuX1.Minimum = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.nuX1.Name = "nuX1";
-            this.nuX1.Size = new System.Drawing.Size(120, 20);
-            this.nuX1.TabIndex = 10;
-            this.nuX1.Value = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.nuX1.ValueChanged += new System.EventHandler(this.nuX1_ValueChanged);
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 317);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "X1";
             // 
             // nuX2
             // 
-            this.nuX2.Location = new System.Drawing.Point(502, 77);
+            this.nuX2.Location = new System.Drawing.Point(38, 356);
             this.nuX2.Maximum = new decimal(new int[] {
             1,
             0,
@@ -263,8 +264,8 @@
             0,
             0});
             this.nuX2.Name = "nuX2";
-            this.nuX2.Size = new System.Drawing.Size(120, 20);
-            this.nuX2.TabIndex = 11;
+            this.nuX2.Size = new System.Drawing.Size(41, 20);
+            this.nuX2.TabIndex = 15;
             this.nuX2.Value = new decimal(new int[] {
             1,
             0,
@@ -272,45 +273,51 @@
             0});
             this.nuX2.ValueChanged += new System.EventHandler(this.nuX2_ValueChanged);
             // 
-            // label4
+            // nuX1
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(476, 38);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(20, 13);
-            this.label4.TabIndex = 12;
-            this.label4.Text = "X1";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(476, 79);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(20, 13);
-            this.label5.TabIndex = 13;
-            this.label5.Text = "X2";
+            this.nuX1.Location = new System.Drawing.Point(36, 315);
+            this.nuX1.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nuX1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nuX1.Name = "nuX1";
+            this.nuX1.Size = new System.Drawing.Size(41, 20);
+            this.nuX1.TabIndex = 14;
+            this.nuX1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nuX1.ValueChanged += new System.EventHandler(this.nuX1_ValueChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(791, 368);
+            this.ClientSize = new System.Drawing.Size(791, 428);
             this.Controls.Add(this.tabControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Теория планирования экспериментов ЛР1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuP)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nuN)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nuP)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nuX1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nuX2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nuX1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -324,16 +331,16 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.NumericUpDown nuX2;
-        private System.Windows.Forms.NumericUpDown nuX1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.NumericUpDown nuP;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button Расчёт;
         private System.Windows.Forms.NumericUpDown nuN;
         private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown nuX2;
+        private System.Windows.Forms.NumericUpDown nuX1;
     }
 }
 
--- TPELab1/Form1.cs
+++ TPELab1/Form1.cs
@@ -10,8 +10,12 @@ namespace TPELab1
         public Form1()
         {
             InitializeComponent();
+            nuX1L.Add(nuX1);
+            nuX2L.Add(nuX2);
         }
 
+        List<NumericUpDown> nuX1L = new List<NumericUpDown>();
+        List<NumericUpDown> nuX2L = new List<NumericUpDown>();
 
         DataTable calcualteFull(int n)
         {
@@ -42,38 +46,60 @@ namespace TPELab1
 
         DataTable calcualteNotFull(int n, int p, int i1, int i2)
         {
-            Random r = new Random();
-            DataTable dt = new DataTable();
-            int[] degs2 = new int[31];
-            degs2[0] = 1;
-            for (int i = 1; i < 31; i++)
-                degs2[i] = degs2[i - 1] * 2;
+            //Random r = new Random();
+            //DataTable dt = new DataTable();
+            //int[] degs2 = new int[31];
+            //degs2[0] = 1;
+            //for (int i = 1; i < 31; i++)
+            //    degs2[i] = degs2[i - 1] * 2;
 
-            dt.Columns.Add("№");
-            for (int i = 0; i < n; i++)
-                dt.Columns.Add("X" + (i + 1));
-            dt.Columns.Add("X" + (i1) + " x X" + (i2));
-            dt.Columns.Add("y");
-            List<int> used = new List<int>();
-            for (int i = 0; i < degs2[n - p]; i++)
+            //dt.Columns.Add("№");
+            //for (int i = 0; i < n; i++)
+            //    dt.Columns.Add("X" + (i + 1));
+            //dt.Columns.Add("X" + (i1) + " x X" + (i2));
+            //dt.Columns.Add("y");
+            //List<int> used = new List<int>();
+            //for (int i = 0; i < degs2[n - p]; i++)
+            //{
+            //    int num = 0;
+            //    do
+            //    {
+            //        num = r.Next(0, degs2[n]);
+            //    } while (used.Contains(num));
+            //    used.Add(num);
+
+            //    object[] row = new object[n + 3];
+            //    row[0] = i + 1;
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        row[j + 1] = ((num) & degs2[n - j - 1]) == 0 ? -1 : 1;
+            //    }
+            //    row[n + 1] = (int)row[i1] * (int)row[i2];
+            //    row[n + 2] = "y";
+            //    dt.Rows.Add(row);
+            //}
+            dataGridView2.Columns.Clear();
+
+            DataTable dt = calcualteFull(n - p);
+            dt.Columns.RemoveAt(dt.Columns.Count - 1);
+
+            for (int j = 0; j < p; j++)
             {
-                int num = 0;
-                do
-                {
-                    num = r.Next(0, degs2[n]);
-                } while (used.Contains(num));
-                used.Add(num);
 
-                object[] row = new object[n + 3];
-                row[0] = i + 1;
-                for (int j = 0; j < n; j++)
+                int j1 = (int)nuX1L[j].Value;
+                int j2 = (int)nuX2L[j].Value;
+
+                dt.Columns.Add("col" + j);
+                for (int i = 0; i < dt.Rows.Count; i++)
                 {
-                    row[j + 1] = ((num) & degs2[n - j - 1]) == 0 ? -1 : 1;
+                    dt.Rows[i][n - p + j + 1] = int.Parse(dt.Rows[i][j1].ToString()) * int.Parse(dt.Rows[i][j2].ToString());
                 }
-                row[n + 1] = (int)row[i1] * (int)row[i2];
-                row[n + 2] = "y";
-                dt.Rows.Add(row);
             }
+
+            dt.Columns.Add("y");
+            for (int i = 0; i < dt.Rows.Count; i++)
+                dt.Rows[i][n + 1] = "y";
+
             return dt;
         }
 
@@ -97,6 +123,13 @@ namespace TPELab1
             int x2 = (int)nuX2.Value;
             DataTable dt = calcualteNotFull(n, p, x1, x2);
             dataGridView2.DataSource = dt;
+
+            for (int j = 0; j < p; j++)
+            {
+                int j1 = (int)nuX1L[j].Value;
+                int j2 = (int)nuX2L[j].Value;
+                dataGridView2.Columns[n - p + j + 1].HeaderText = dataGridView2.Columns[j1].HeaderText + " x" + dataGridView2.Columns[j2].HeaderText;
+            }
         }
         private void button2_Click(object sender, EventArgs e)
         {
@@ -105,39 +138,145 @@ namespace TPELab1
 
         private void nuN_ValueChanged(object sender, EventArgs e)
         {
-            if (nuP.Value >= nuN.Value)
-                nuP.Value = nuN.Value;
-            nuP.Maximum = nuN.Value;
+            if (nuP.Value+2 >= nuN.Value)
+                nuP.Value = nuN.Value-2;
+            nuP.Maximum = nuN.Value-2;
 
-            if (nuX1.Value >= nuN.Value)
+          /*  if (nuX1.Value >= nuN.Value)
                 nuX1.Value = nuN.Value;
             nuX1.Maximum = nuN.Value;
 
             if (nuX2.Value >= nuN.Value)
                 nuX2.Value = nuN.Value;
             nuX2.Maximum = nuN.Value;
+            */
 
-            CalculateNotFull();
+            for (int i = 0; i < nuX1L.Count; i++)
+            {
+                if (nuX1L[i].Value >= nuN.Value-nuP.Value)
+                    nuX1L[i].Value = nuN.Value-nuP.Value;
+                nuX1L[i].Maximum = nuN.Value-nuP.Value;
+
+                if (nuX2L[i].Value >= nuN.Value-nuP.Value)
+                    nuX2L[i].Value = nuN.Value-nuP.Value;
+                nuX2L[i].Maximum = nuN.Value-nuP.Value;
+            }
+
+           // CalculateNotFull();
         }
 
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
-            CalculateFull();
+           // CalculateFull();
+        }
+
+
+        NumericUpDown Clone(NumericUpDown src)
+        {
+            NumericUpDown res = new NumericUpDown();
+            res.Width = src.Width;
+            res.Height = src.Height;
+            res.Top = src.Top;
+            res.Left = src.Left + src.Width + 20;
+
+            res.Maximum = src.Maximum;
+            res.Minimum = src.Minimum;
+            res.Value = 1;
+            tabPage2.Controls.Add(res);
+
+            return res;
         }
 
         private void nuP_ValueChanged(object sender, EventArgs e)
         {
-            CalculateNotFull();
+
+
+            if (nuP.Value > nuX1L.Count)
+            {
+                if (nuP.Value == 1)
+                {
+                    nuX1.Visible = true;
+                    nuX2.Visible = true;
+                }
+                else
+                {
+                    NumericUpDown nu1 = Clone(nuX1L[nuX1L.Count - 1]);
+                    NumericUpDown nu2 = Clone(nuX2L[nuX2L.Count - 1]);
+                    nu1.ValueChanged += nuX1_ValueChanged;
+                   nu2.ValueChanged += nuX2_ValueChanged;
+                    nuX1L.Add(nu1);
+                    nuX2L.Add(nu2);
+
+                   // nu1.ChangeUICues += nuX1_ValueChanged;
+                   // nu2.ChangeUICues += nuX2_ValueChanged;
+                }
+            }
+            else
+            {
+                if (nuP.Value == 0)
+                {
+                    nuX1.Visible = false;
+                    nuX2.Visible = false;
+                }
+                else
+                {
+                    tabPage2.Controls.Remove(nuX1L[nuX1L.Count - 1]);
+                    tabPage2.Controls.Remove(nuX2L[nuX2L.Count - 1]);
+
+                    nuX1L.RemoveAt(nuX1L.Count - 1);
+                    nuX2L.RemoveAt(nuX2L.Count - 1);
+                }
+            }
+
+            for (int i = 0; i < nuX1L.Count; i++)
+            {
+                nuX1L[i].Maximum = nuN.Value-nuP.Value;
+                nuX2L[i].Maximum = nuN.Value-nuP.Value;
+            }
+
+           // CalculateNotFull();
         }
 
+        void calculateP(int n, int p)
+        {
+            for (int j = 0; j < p; j++)
+            {
+                int j1 = (int)nuX1L[j].Value;
+                int j2 = (int)nuX2L[j].Value;
+
+                dataGridView2.Columns[n - p + j + 1].HeaderText = dataGridView2.Columns[j1].HeaderText + " x"+ dataGridView2.Columns[j2].HeaderText;
+                for (int i = 0; i < dataGridView2.RowCount; i++)
+                {
+                    dataGridView2[n - p + j + 1, i].Value = int.Parse(dataGridView2[j1, i].Value.ToString()) * int.Parse(dataGridView2[j2, i].Value.ToString());
+                }
+            }
+
+        }
         private void nuX1_ValueChanged(object sender, EventArgs e)
         {
-            CalculateNotFull();
+            int n = (int)nuN.Value;
+            int p = (int)nuP.Value;
+           // calculateP(n, p);
         }
 
         private void nuX2_ValueChanged(object sender, EventArgs e)
         {
+
+            int n = (int)nuN.Value;
+            int p = (int)nuP.Value;
+           // calculateP(n, p);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CalculateFull();
             CalculateNotFull();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, UICuesEventArgs e)
+        {
+
         }
     }
 }
